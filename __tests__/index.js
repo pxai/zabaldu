d20317a5800ae25cbed8f129b031c96ba590a7a1
pxai <pello.altadill@gluckgames.com>
2023-03-18 17:52:01 +0100

    Remove jest

--- __tests__/index.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Home from "../src/pages/index";
-
-test("Check for Getting Started Text", () => {
-  const { getByText } = render(<Home />);
-  expect(getByText("Latest")).toBeInTheDocument();
-});
