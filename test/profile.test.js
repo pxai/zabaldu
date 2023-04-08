bbe07a410f4940ca6bf705913e0230ff2ac63e37
pxai <pello.altadill@gmail.com>
2023-04-08 09:12:24 +0200

    Added circle ci

--- test/profile.test.js
+++ test/profile.test.js
@@ -1,21 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import Profile from '../src/pages/profile'
 import { useSession } from "next-auth/react";
-import mockRouter from 'next-router-mock';
-jest.mock('next/router', () => require('next-router-mock'));
 
- jest.mock('react-i18next', () => ({
-  // this mock makes sure any components using the translate hook can use it without a warning being shown
-  useTranslation: () => {
-    return {
-      t: (str) => str,
-      i18n: {
-        changeLanguage: () => new Promise(() => {}),
-      },
-    };
-  },
-}));
-jest.mock('next-auth/react');
 
 describe('Profile',  () => {
   it('does not render headiig when session is empty', async () => {
--- test/setupJestMock.js
+++ test/setupJestMock.js
@@ -1,3 +1,9 @@
+import mockRouter from 'next-router-mock';
+jest.mock('next/router', () => require('next-router-mock'));
+
+
+jest.mock('next-auth/react');
+
 jest.mock('react-i18next', () => ({
     // this mock makes sure any components using the translate hook can use it without a warning being shown
     useTranslation: () => {
